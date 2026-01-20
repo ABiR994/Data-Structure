6bb5d99f7c5e7bfac9bec4221c7eb638f3d97ba2
ABiR994 <salemnurabir2@gmail.com>
2026-01-21 04:21:59 +0600

    print 1 to n

---
+++ 2. Conditional Statements & Loops/print_1_to_n.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Enter the value of n: ";
+    int n;
+    cin >> n;
+
+    for(int i = 1; i <= n; i++) {
+        cout << i << "\t";
+    }
+    cout << endl;
+    return 0;
+}
