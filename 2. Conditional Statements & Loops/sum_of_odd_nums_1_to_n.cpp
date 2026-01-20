f4e6c7220b76fdc7dae327b3b6f2c6b6b43c4eaa
ABiR994 <salemnurabir2@gmail.com>
2026-01-21 04:29:18 +0600

    sum of all odd numbers from 1 to n

---
+++ 2. Conditional Statements & Loops/sum_of_odd_nums_1_to_n.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Enter the value of n: ";
+    int n;
+    cin >> n;
+
+    int sum = 0;
+    for(int i = 1; i <= n; i++) {
+        if(i % 2 != 0) {
+            sum += i;
+        }
+    }
+    cout << "The sum of odd numbers from 1 to " << n << " is: " << sum << endl;
+    return 0;
+}
