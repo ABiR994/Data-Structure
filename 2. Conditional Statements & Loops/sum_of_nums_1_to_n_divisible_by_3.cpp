cd269d14e2d0bba24bb373be76dafd6c83f88b75
ABiR994 <salemnurabir2@gmail.com>
2026-01-21 04:55:52 +0600

    sum of numbers from 1 to n divisible by 3

---
+++ 2. Conditional Statements & Loops/sum_of_nums_1_to_n_divisible_by_3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Enter the value of n: ";
+    int n;
+    cin >> n;
+
+    int sum = 0;
+
+    for(int i = 1; i <= n; i++) {
+        if(i % 3 == 0) {
+            sum += i;
+        }
+    }
+
+    cout << "Sum of numbers from 1 to " << n << " divisible by 3 is: " << sum << endl;
+    return 0;
+}
