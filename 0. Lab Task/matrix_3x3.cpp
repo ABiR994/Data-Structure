c180cf4197235b5f02dc44b0b951d1ed3eea6c3b
ABiR994 <salemnurabir2@gmail.com>
2026-02-10 09:35:36 +0600

    3x3 matrix

---
+++ 0. Lab Task/matrix_3x3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int mat[3][3];
+    cout << "Enter 9 elements for the matrix:" << endl;
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 3; j++) {
+            cin >> mat[i][j];
+        }
+    }
+
+    cout << "The 3x3 matrix is:" << endl;
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 3; j++) {
+            cout << mat[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
