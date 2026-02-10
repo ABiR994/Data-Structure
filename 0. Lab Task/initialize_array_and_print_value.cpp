4ed660a32bf94a07f8d7d9f5a489e5d7934c7dff
ABiR994 <salemnurabir2@gmail.com>
2026-02-10 08:51:11 +0600

    initialize array and print value

---
+++ 0. Lab Task/initialize_array_and_print_value.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[5];
+    arr[0] = 10;
+    arr[1] = 20;
+    arr[2] = 30;
+    arr[3] = 40;
+    arr[4] = 50;
+
+    cout << "All array elements: " << endl;
+    for (int i = 0; i < 5; i++) {
+        cout << "arr[" << i << "] = " << arr[i] << endl;
+    }
+
+    return 0;
+}
