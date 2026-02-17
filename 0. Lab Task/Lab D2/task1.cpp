69f81d6318fc1a8edf9def69a35f3d4cf848392d
ABiR994 <salemnurabir2@gmail.com>
2026-02-17 08:38:21 +0600

    Transpose matrix

---
+++ 0. Lab Task/Lab D2/task1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+void input(int mat[3][4]) {
+    cout << "Enter 12 elements for the matrix:" << endl;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 4; j++) {
+            cin >> mat[i][j];
+        }
+    }
+}
+
+
+void transpose(int mat[3][4]) {
+    cout << "The transpose matrix is:" << endl;
+    for (int j = 0; j < 4; j++) {
+        for (int i = 0; i < 3; i++) {
+            cout << mat[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int mat[3][4];
+    input(mat);
+    cout << endl;
+    transpose(mat);
+    return 0;
+}
