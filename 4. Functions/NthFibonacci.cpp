f52c738f3cc0fe5abe0595db07143f2db4a90db3
ABiR994 <salemnurabir2@gmail.com>
2026-05-15 22:49:04 +0600

    Add implementation of Fibonacci number calculation with user input

---
+++ 4. Functions/NthFibonacci.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+void fibonacci(int n) {
+    if (n <= 0) {
+        cout << "Please enter a positive integer." << endl;
+        return;
+    } else if (n == 1) {
+        cout << n << "th Fibonacci number is 0" << endl;
+        return;
+    } else if (n == 2) {
+        cout << n << "th Fibonacci number is 1" << endl;
+        return;
+    }
+    int a = 0, b = 1;
+    int temp;
+    for(int i = 3; i <= n; i++) {
+        temp = a + b;
+        a = b;
+        b = temp;
+    }
+    cout << n << "th Fibonacci number is " << b << endl;
+}
+
+int main() {
+    int n;
+    cout << "Enter a positive integer: ";
+    cin >> n;
+    fibonacci(n);
+    return 0;
+}
